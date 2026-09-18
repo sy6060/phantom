7be49923292235c4e35c277034227d005c9f03e6
sanvi yadav <sanvi.25b01010431.abes.ac.in>
2026-09-18 09:59:27 +0530

    .

---
+++ p3.py
@@ -0,0 +1,6 @@
+#check whether number lies bw 1 to 100
+num = int(input("Enter a number: "))
+if 1 <= num <= 100:
+    print("Number is between 1 and 100")
+else:
+    print("Number is not between 1 and 100")
